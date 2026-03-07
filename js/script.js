d033292d78ca87e34608ae7060e98f8d030462c5
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2026-03-07 22:53:48 +0530

    did maney major changes like added navebar and changed theam and all

--- js/script.js
+++ js/script.js
@@ -10,93 +10,134 @@ var typed = new Typed(".typing", {
     backSpeed: 25,
     loop: true
 });
-/*============Aside========*/
+/* ============ Aside Navigation ============ */
+
 const nav = document.querySelector(".nav"),
       navList = nav.querySelectorAll("li"),
       totalNavlist = navList.length,
       allSection = document.querySelectorAll(".section"),
       totalSection = allSection.length;
-      for (let i = 0; i < totalNavlist; i++)
-      {
-     
-        const a = navList[i].querySelector("a");
-        a.addEventListener("click", function()
-        {
-            removeBackSection();
-            for (let j = 0; j < totalNavlist; j++)
-            {
-                 if(navList[j].querySelector("a").classList.contains("active"))
-                {
-                  addBackSection(j);
-                   // allSection[j].classList.add("back-section");
-                }
-                navList[j].querySelector("a").classList.remove("active");
 
-            }   
-            this.classList.add("active")
-            showSection(this);
-            if(window.innerWindh < 1200)
-            {
-                asideSectionToggleBtn();
+/* ===== Navigation Click ===== */
 
-            }
-        })
-      } 
-      function removeBackSection()
-      {
-        for(let i =0; i<totalSection; i++)
-        {
-          allSection[i].classList.remove("back-section");
-        }
-      } 
-      function addBackSection(num) {
-  if (num !== undefined) {
-    allSection[num].classList.add("back-section");
-  }
-}
-      function showSection(element)
-      {
-          for (let i = 0; i < totalSection; i++)
-          {
-            allSection[i].classList.remove("active");
-          }
-          const target = element.getAttribute("href").split("#")[1];
-          document.querySelector("#" + target).classList.add("active")
-      }
-      function updateNav(element)
-      {
-        for (let i = 0; i < totalNavlist; i++) 
-        {
-            // The logic was incomplete. It should check if the href attribute of the element
-            // matches the target of the nav item.
-            const target = element.getAttribute("href").split("#")[1];
-            if (target === navList[i].querySelector("a").getAttribute("href").split("#")[1]) {
-              navList[i].querySelector("a").classList.add("active");
-            } else {
-              navList[i].querySelector("a").classList.remove("active");
-            }
-        }
-      }
-      document.querySelector(".hire-me").addEventListener("click", function()
-      {
-        const sectionIndex = parseInt(this.getAttribute("data-section-index"));
-        showSection(this);
-        updateNav(this);
+for (let i = 0; i < totalNavlist; i++)
+{
+    const a = navList[i].querySelector("a");
+
+    a.addEventListener("click", function ()
+    {
         removeBackSection();
-        addBackSection(sectionIndex);
-      })
-const navTogglerBtn = document.querySelector(".toggler");
-            aside = document.querySelector(".aside");
-            navTogglerBtn.addEventListener("click", ()=>
+
+        for (let j = 0; j < totalNavlist; j++)
+        {
+            if (navList[j].querySelector("a").classList.contains("active"))
             {
-                asideSectionToggleBtn();
-            })
-            function asideSectionToggleBtn()
-            {
-                aside.classList.toggle("open");
-                navTogglerBtn.classList.toggle("open");
-                for (let i = 0; i < totalSection; i++)
-                {
-                    allSection[i].classList.toggle("open");
-                }
+                addBackSection(j);
             }
+
+            navList[j].querySelector("a").classList.remove("active");
+        }
+
+        this.classList.add("active");
+        showSection(this);
+
+        if (window.innerWidth < 1200)
+        {
+            asideSectionToggleBtn();
+        }
+    });
+}
+
+
+/* ===== Remove Back Section ===== */
+
+function removeBackSection()
+{
+    for (let i = 0; i < totalSection; i++)
+    {
+        allSection[i].classList.remove("back-section");
+    }
+}
+
+
+/* ===== Add Back Section ===== */
+
+function addBackSection(num)
+{
+    if (num !== undefined)
+    {
+        allSection[num].classList.add("back-section");
+    }
+}
+
+
+/* ===== Show Active Section ===== */
+
+function showSection(element)
+{
+    for (let i = 0; i < totalSection; i++)
+    {
+        allSection[i].classList.remove("active");
+    }
+
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active");
+}
+
+
+/* ===== Update Navigation ===== */
+
+function updateNav(element)
+{
+    for (let i = 0; i < totalNavlist; i++)
+    {
+        const target = element.getAttribute("href").split("#")[1];
+
+        if (target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
+        {
+            navList[i].querySelector("a").classList.add("active");
+        }
+        else
+        {
+            navList[i].querySelector("a").classList.remove("active");
+        }
+    }
+}
+
+
+/* ===== Hire Me Button ===== */
+
+document.querySelector(".hire-me").addEventListener("click", function ()
+{
+    const sectionIndex = parseInt(this.getAttribute("data-section-index"));
+
+    showSection(this);
+    updateNav(this);
+    removeBackSection();
+    addBackSection(sectionIndex);
+});
+
+
+/* ===== Aside Toggle Button ===== */
+
+const navTogglerBtn = document.querySelector(".nav-toggler");
+const aside = document.querySelector(".aside");
+
+navTogglerBtn.addEventListener("click", () =>
+{
+    asideSectionToggleBtn();
+});
+
+
+/* ===== Toggle Sidebar ===== */
+
+function asideSectionToggleBtn()
+{
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+
+    for (let i = 0; i < totalSection; i++)
+    {
+        allSection[i].classList.toggle("open");
+    }
+}
